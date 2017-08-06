b0610d4e91089d1642969c4b6509cad0ebd9fc64
jordiprats <jordi.prats@gmail.com>
2017-08-06 23:43:42 +0200

    multiauth

---
+++ eypconf/app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class AdminLoginController extends Controller
+{
+    public function __construct()
+    {
+      $this->middleware('guest:admin');
+    }
+
+    public function showLoginForm()
+    {
+      return view('auth.admin-login');
+    }
+
+    public function login(Request $request)
+    {
+      //validate
+      $this->validate($request, [
+        'email' => 'required|email',
+        'password' => 'required|min:6'
+      ]);
+
+      $credentials = [
+        'email' => $request->email,
+        'password' => $request->password,
+      ];
+
+      if(Auth::guard('admin')->attempt($credentials, $request->remember))
+      {
+        return redirect()->intended(route('admin.dashboard'));
+      }
+
+      return redirect()->back()->withInput($request->only('email', 'remember'));
+
+    }
+}
