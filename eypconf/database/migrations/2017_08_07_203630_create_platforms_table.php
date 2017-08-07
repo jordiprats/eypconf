1fdd11258899b9f16f971eb8da7b53383873c8d7
jordiprats <jordi.prats@gmail.com>
2017-08-07 23:10:57 +0200

    model platform

---
+++ eypconf/database/migrations/2017_08_07_203630_create_platforms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlatformsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('platforms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->string('eyp_userid');
+            $table->string('eyp_magic_hash');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('platforms');
+    }
+}
--- eypconf/resources/views/home.blade.php
+++ eypconf/resources/views/home.blade.php
@@ -8,7 +8,39 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                  <ul>
+                    <li><h2>Platform</h2>
+                    <p>platform description</p>
+                    <p>platform userid / magic hash?</p>
+                    <h3>Environments</h3>
+                    <ul>
+                      <li>...</li>
+                    </ul>
+                    <h3>Server types</h3>
+                    <ul>
+                      <li>...</li>
+                    </ul>
+                    <h3>Server groups</h3>
+                    <ul>
+                      <li>...</li>
+                    </ul></li>
+
+                    <li><h2>Platform</h2>
+                    <p>platform description</p>
+                    <p>platform userid / magic hash?</p>
+                    <h3>Environments</h3>
+                    <ul>
+                      <li>...</li>
+                    </ul>
+                    <h3>Server types</h3>
+                    <ul>
+                      <li>...</li>
+                    </ul>
+                    <h3>Server groups</h3>
+                    <ul>
+                      <li>...</li>
+                    </ul></li>
+                  </ul>
                 </div>
             </div>
         </div>
