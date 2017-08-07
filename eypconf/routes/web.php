c8c6f68f510a6938790cba653479cf7ddd5f5d1b
jordiprats <jordi.prats@gmail.com>
2017-08-07 23:28:41 +0200

    crud

--- eypconf/routes/web.php
+++ eypconf/routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/platforms', 'PlatformController');
 
 Route::prefix('admin')->group(function () {
   Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
