99f81bacdfffa3a5d8ba548b2250317a9e058cbd
jordiprats <jordi.prats@gmail.com>
2017-08-08 09:18:44 +0200

    creant plataformes

--- eypconf/app/Http/Controllers/PlatformController.php
+++ eypconf/app/Http/Controllers/PlatformController.php
@@ -23,7 +23,7 @@ class PlatformController extends Controller
      */
     public function create()
     {
-        //
+        return view('platforms.create');
     }
 
     /**
@@ -34,7 +34,15 @@ class PlatformController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate
+        $this->validate($request, array(
+          'platform_name' => 'required|string|max:255',
+          'description' => 'string|max:255',
+        ));
+
+        // store in the db
+
+        // redirect
     }
 
     /**
--- eypconf/app/Platform.php
+++ eypconf/app/Platform.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Platform extends Model
 {
     //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
