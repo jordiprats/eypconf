be3b1a6b5b7e39d4cec9a9e4d4d9cce2ff9b177e
jordiprats <jordi.prats@gmail.com>
2017-08-11 08:32:30 +0200

    boto accions per plataforma

--- eypconf/resources/views/platforms/show.blade.php
+++ eypconf/resources/views/platforms/show.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
   @if(Auth::user()==$user)
   <div>
-    {!! Form::open(['route' => 'servertypes.create', 'method' => 'get']) !!}
+    {{-- {!! Form::open(['route' => 'servertypes.create', 'method' => 'get']) !!}
       {{ Form::hidden('platform_id', $platform->id) }}
       {{ Form::submit('Create server type', array('class'=>'btn btn-primary', 'style'=>'float:right')) }}
     {!! Form::close() !!}
@@ -15,7 +15,15 @@
     {!! Form::open(['route' => 'environments.create', 'method' => 'get']) !!}
       {{ Form::hidden('platform_id', $platform->id) }}
       {{ Form::submit('Create environment', array('class'=>'btn btn-primary', 'style'=>'float:right')) }}
-    {!! Form::close() !!}
+    {!! Form::close() !!} --}}
+    <div class="dropdown" style="float: right;">
+      <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown">Actions <span class="caret"></span></button>
+      <ul class="dropdown-menu" role="menu">
+        <li><a href="{{ route('environments.create') }}">Create environment</a></li>
+        <li><a href="{{ route('servertypes.create') }}">Create server type</a></li>
+        <li><a href="{{ route('servergroups.create') }}">Create server group</a></li>
+      </ul>
+    </div>
   </div>
   @endif
   <h1>Platform {{ $platform->platform_name }}</h1>
