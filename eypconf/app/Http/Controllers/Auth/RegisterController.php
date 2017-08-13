60aa97795de5bd20f96a1e13ceeb5828ffaaa613
jordiprats <jordi.prats@gmail.com>
2017-08-13 23:23:57 +0200

    afegits slugs

--- eypconf/app/Http/Controllers/Auth/RegisterController.php
+++ eypconf/app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
         'name' => $data['name'],
         'email' => $data['email'],
         'username' => $data['username'],
+        'slug' => str_slug($data['username'], '-'),
         'password' => bcrypt($data['password']),
     ]);
   }
