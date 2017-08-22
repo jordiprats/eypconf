685b3e40f1c35388b5fb786c892efa470ac9d570
jordiprats <jordi.prats@gmail.com>
2017-08-22 20:56:42 +0200

    comandes docker

--- eypconf/app/Jobs/GitInit.php
+++ eypconf/app/Jobs/GitInit.php
@@ -35,9 +35,9 @@ class GitInit implements ShouldQueue
   {
     // creem contenidor de dades
     // docker run -d -n $platform->id -t eyp/git-repo
-    echo exec("docker run -d -n ".$this->platform->id." -t eyp/git-repo");
+    echo exec("docker run -d --name platformid_".$this->platform->id." -t eyp/git-repo");
 
     // creem repo pel contenidor
-    echo exec("docker run --volumes-from ".$this->platform->id." -t eyp/git git -C /repo init");
+    echo exec("docker run --volumes-from platformid_".$this->platform->id." -t eyp/git -- git -C /repo init");
   }
 }
