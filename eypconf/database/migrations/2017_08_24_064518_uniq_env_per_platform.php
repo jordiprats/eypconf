12dd0237bed8cdb9991c435c0cc4cd881c1766a7
jordiprats <jordi.prats@gmail.com>
2017-08-27 22:20:23 +0200

    lint

--- eypconf/database/migrations/2017_08_24_064518_uniq_env_per_platform.php
+++ eypconf/database/migrations/2017_08_24_064518_uniq_env_per_platform.php
@@ -6,27 +6,27 @@ use Illuminate\Database\Migrations\Migration;
 
 class UniqEnvPerPlatform extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('environments', function (Blueprint $table) {
-            $table->unique(['platform_id', 'slug'], 'uniq_envslug_per_platform');
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('environments', function (Blueprint $table) {
+      $table->unique(['platform_id', 'slug'], 'uniq_envslug_per_platform');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('environments', function (Blueprint $table) {
-          $table->dropUnique('uniq_envslug_per_platform'); 
-        });
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('environments', function (Blueprint $table) {
+      $table->dropUnique('uniq_envslug_per_platform');
+    });
+  }
 }
