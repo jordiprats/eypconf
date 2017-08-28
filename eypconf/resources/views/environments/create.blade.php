e841f4262cd01f48bfd39c1314b0a35774dfd88e
jordiprats <jordi.prats@gmail.com>
2017-08-28 08:21:13 +0200

    gestio errors environments

--- eypconf/resources/views/environments/create.blade.php
+++ eypconf/resources/views/environments/create.blade.php
@@ -6,7 +6,15 @@
   <hr/>
   {!! Form::open(['route' => ['environments.store', $user->username, $platform->platform_name]]) !!}
     {{ Form::label('environment_name', 'Environment name:') }}
+    <span class='help-inline'>Although it can be any string, it will be converted to ASCII only characters ("pre producció" - "pre-produccio")</span>
     {{ Form::text('environment_name', null, array('class' => 'form-control')) }}
+    @foreach ($errors->get('environment_name') as $error)
+      <div class='alert alert-danger'>{{ $error }}</span>
+    @endforeach
+    @foreach ($errors->get('slug') as $error)
+      {{-- mixed str_replace ( mixed $search , mixed $replace , mixed $subject [, int &$count ] ) --}}
+      <div class='alert alert-danger'>{{ str_replace('slug','environment name', $error) }}</div>
+    @endforeach
     {{ Form::label('description', 'Short description:') }}
     {{ Form::text('description', null, array('class' => 'form-control')) }}
 
