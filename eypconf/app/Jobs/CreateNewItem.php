cdb7a649eff5b4ab783006667bf2f65cdf8e0dae
jordiprats <jordi.prats@gmail.com>
2017-08-31 07:43:32 +0200

    mkdir items

--- eypconf/app/Jobs/CreateNewItem.php
+++ eypconf/app/Jobs/CreateNewItem.php
@@ -29,8 +29,8 @@ class CreateNewItem implements ShouldQueue
   public function __construct(Platform $platform, string $dir, string $name)
   {
     $this->platform = $platform;
-    $this->dir = $dir;
-    $this->name = $name;
+    $this->dir = escapeshellcmd($dir);
+    $this->name = escapeshellcmd($name);
 
     if(User::where('id', $platform->user_id)->count() == 1)
     {
@@ -60,16 +60,34 @@ class CreateNewItem implements ShouldQueue
       throw new Exception ('platform\'s git have not been created yet');
     echo "b\n";
 
-    echo 'dir: /repo/'.$this->dir.'/'.$this->name."\n";
+    echo 'mkdir: /repo/'.$this->dir.'/'.$this->name."\n";
 
     //creem nou item
+    $cmd="docker run -i --volumes-from platformid_".$this->platform->id." -t eyp/git mkdir -p '/repo/".$this->dir.'/'.$this->name."'";
+    echo "mkdir /".$cmd."/: ".exec($cmd)."\n";
+
+    //gitkeep
+    $cmd="docker run -i --volumes-from platformid_".$this->platform->id." -t eyp/git touch '/repo/".$this->dir.'/'.$this->name."/.gitkeep'";
+    echo "gitkeep /".$cmd."/: ".exec($cmd)."\n";
 
     // afegim tots els fitxers
     $cmd="docker run -i --volumes-from platformid_".$this->platform->id." -t eyp/git git -C /repo add --all";
     echo "add all /".$cmd."/: ".exec($cmd)."\n";
 
     // commit inicial
-    $cmd="docker run -i --volumes-from platformid_".$this->platform->id." -t eyp/git git -C /repo commit -m 'template'";
+    $cmd="docker run -i --volumes-from platformid_".$this->platform->id." -t eyp/git git -C /repo commit -m 'mkdir ".$this->dir.' // '.$this->name."'";
     echo "commit template /".$cmd."/: ".exec($cmd)."\n";
+
+    $returncode=666;
+    $output=[];
+    $cmd="docker run -i --volumes-from platformid_".$this->platform->id." -t eyp/git git -C /repo status";
+    echo "git status/".$cmd."/: ".exec($cmd, $output, $returncode)."\n";
+    if($returncode!=0) throw new Exception ('error git status'); else echo "git OK\n";
+
+    $returncode=666;
+    $output=[];
+    $cmd="docker run -i --volumes-from platformid_".$this->platform->id." -t eyp/git [ -f '/repo/".$this->dir.'/'.$this->name."/.gitkeep' ]";
+    echo "readme precense/".$cmd."/: ".exec($cmd, $output, $returncode)."\n";
+    if($returncode!=0) throw new Exception ('mkdir failed'); else echo "mkdir ok\n";
   }
 }
