6e4502fba4ceb56ee380527bdadee724d355fd1b
jordiprats <jordi.prats@gmail.com>
2017-09-04 07:14:55 +0200

    mes status

--- eypconf/database/migrations/2017_09_04_051209_st_status.php
+++ eypconf/database/migrations/2017_09_04_051209_st_status.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EnvironmentStatus extends Migration
+class STStatus extends Migration
 {
   /**
    * Run the migrations.
@@ -13,7 +13,7 @@ class EnvironmentStatus extends Migration
    */
   public function up()
   {
-    Schema::table('environments', function (Blueprint $table) {
+    Schema::table('servertypes', function (Blueprint $table) {
       $table->integer('status')->default('0');
     });
   }
@@ -25,7 +25,7 @@ class EnvironmentStatus extends Migration
    */
   public function down()
   {
-    Schema::table('environments', function (Blueprint $table) {
+    Schema::table('servertypes', function (Blueprint $table) {
       $table->dropColumn('status');
     });
   }
