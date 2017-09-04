34edc0b92b1b5838ed188cd73de0aeabf2eccbf4
jordiprats <jordi.prats@gmail.com>
2017-09-04 08:09:13 +0200

    estil

--- eypconf/resources/views/welcome.blade.php
+++ eypconf/resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-  news
+  users
   <ul>
     @foreach ($users as $user)
     <li><a href="{{ route('show.eyp.user', ['user' => $user->username ]) }}">{{ $user->username }}</a></li>
