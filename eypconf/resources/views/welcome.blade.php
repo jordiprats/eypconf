d25c413a2605b5323ecd8e54a8f56dc71e691db0
jordiprats <jordi.prats@gmail.com>
2017-09-04 08:08:54 +0200

    llistat users

--- eypconf/resources/views/welcome.blade.php
+++ eypconf/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   news
   <ul>
     @foreach ($users as $user)
-    <li>{{ $user->username }}</li>
+    <li><a href="{{ route('show.eyp.user', ['user' => $user->username ]) }}">{{ $user->username }}</a></li>
     @endforeach
   </ul>
 </div>
