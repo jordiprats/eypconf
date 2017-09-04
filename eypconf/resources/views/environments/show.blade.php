9864d74f130a668a16177c196ae4e61a8f0c1221
jordiprats <jordi.prats@gmail.com>
2017-09-04 07:41:45 +0200

    estil

--- eypconf/resources/views/environments/show.blade.php
+++ eypconf/resources/views/environments/show.blade.php
@@ -4,7 +4,6 @@
 <div class="container">
   <h1><a href="{{ route('show.eyp.user', ['user' => $user->username ]) }}">{{ $user->username }}</a> / <a href="{{ route('show.eyp.user.platform', ['user' => $user->username, 'platform' => $platform->slug ]) }}">{{ $platform->platform_name }}</a> / environment {{ $environment->environment_name }} @if($environment->status!=1) <i class="fa fa-circle-o-notch fa-spin"></i> @endif</h1>
   <hr/>
-
   <p>{{ $environment->description }}</p>
 </div>
 @endsection
