f754d9940146ac4aabaaed1457a13cf167193b28
jordiprats <jordi.prats@gmail.com>
2017-09-05 07:54:53 +0200

    rename dashboard

--- eypconf/resources/views/home.blade.php
+++ eypconf/resources/views/home.blade.php
@@ -5,8 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+                <div class="panel-heading">{{ $user->username }} ({{ $user->name }})</div>
                 <div class="panel-body">
                   @if(Auth::user()==$user)
                   {!! Form::open(['route' => 'platforms.create', 'method' => 'get']) !!}
