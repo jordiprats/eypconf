10f8b96c948a8e53917db5c1a3a83b5e5d8e0b79
jordiprats <jordi.prats@gmail.com>
2017-09-07 23:16:24 +0200

    estil show env

--- eypconf/resources/views/environments/show.blade.php
+++ eypconf/resources/views/environments/show.blade.php
@@ -2,15 +2,20 @@
 
 @section('content')
 <div class="container">
-  <h1><ol class="breadcrumb">
-    <li class="breadcrumb-item">@include('breadcrumbs.user')</li>
-    <li class="breadcrumb-item">@include('breadcrumbs.platform')</li>
-    <li class="breadcrumb-item active">environment {{ $environment->environment_name }}</li>
-  </ol></h1>
-  {{--  @if($environment->status!=1) <i class="fa fa-circle-o-notch fa-spin"></i> @endif --}}
-  <div> <!-- contenidor accions -->
+  <div class="card">
+    <h1><ol class="breadcrumb">
+      <li class="breadcrumb-item">@include('breadcrumbs.user')</li>
+      <li class="breadcrumb-item">@include('breadcrumbs.platform')</li>
+      <li class="breadcrumb-item active">environment {{ $environment->environment_name }}</li>
+    </ol></h1>
   </div>
   <hr/>
+  <div style="float: right;">
+
+      <i class="fa fa-trash" aria-hidden="true"></i>
+
+  </div>
+  {{--  @if($environment->status!=1) <i class="fa fa-circle-o-notch fa-spin"></i> @endif --}}
   <p>{{ $environment->description }}</p>
 </div>
 @endsection
